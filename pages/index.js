1893aadf86fd4b758a2cd86d5ecd1b39620330de
Arkadiusz Cios <arek123113@gmail.com>
2021-01-14 00:21:23 +0100

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image';
+
+export default function Home() {
+  return (
+    <Container>
+      <h1>
+        🪐🪐🪐🪐🪐🪐🪐🪐🪐🪐🪐🪐🪐🪐🪐🪐🪐🪐🪐🪐🪐🪐🪐🪐🪐🪐🪐🪐🪐🪐🪐🪐🪐🪐🪐🪐🪐🪐🪐🪐🪐🪐🪐🪐
+      </h1>
+      <Image className="mx-auto d-block" style={{ maxWidth: '600px' }} src="https://images.emojiterra.com/google/android-11/share/1fa90.jpg" fluid />
+      <h1>
+        🪐🪐🪐🪐🪐🪐🪐🪐🪐🪐🪐🪐🪐🪐🪐🪐🪐🪐🪐🪐🪐🪐🪐🪐🪐🪐🪐🪐🪐🪐🪐🪐🪐🪐🪐🪐🪐🪐🪐🪐🪐🪐🪐🪐
+      </h1>
+      wow to saturn elo <br />
+      <Image className="mx-auto d-block" style={{ maxWidth: '600px' }} src="https://media4.giphy.com/media/3o7buctjKD8g5r4544/giphy.gif" />
+    </Container>
+  )
+}
